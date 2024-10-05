687f55dbdeb9754cc22b3ad402aa024b03f010a1
Andrew Kay <warfreak2@gmail.com>
2024-10-05 21:22:28 +0100

    Unify serving of static files

--- backend/src/routes/index.rs
+++ backend/src/routes/index.rs
@@ -1,26 +1,21 @@
-use actix_files::NamedFile;
-use actix_web::{get, web, Responder};
+use actix_files::{Files, NamedFile};
+use actix_web::{get, web::ServiceConfig, Responder};
 
 use crate::result::Result;
 
 /// Declares routes for serving static content.
-pub fn declare_routes(conf: &mut web::ServiceConfig) {
-    conf.service(index)
-        .service(app_js)
-        .service(style_css);
+pub fn declare_routes(conf: &mut ServiceConfig) {
+    let files_service = Files::new("/", "./static")
+        .index_file("index.html")
+        // `index.html` is served on the `/` route; do not give it a second
+        // route.
+        .path_filter(|path, _| path.as_os_str() != "index.html");
+    
+    conf.service(files_service)
+        .service(index);
 }
 
 #[get("/")]
 async fn index() -> Result<impl Responder> {
     Ok(NamedFile::open_async("static/index.html").await?)
-}
-
-#[get("/app.js")]
-async fn app_js() -> Result<impl Responder> {
-    Ok(NamedFile::open_async("static/app.js").await?)
-}
-
-#[get("/style.css")]
-async fn style_css() -> Result<impl Responder> {
-    Ok(NamedFile::open_async("static/style.css").await?)
 }
--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -1,10 +1,12 @@
+use actix_web::web::ServiceConfig;
+
 mod auth;
 mod index;
 mod srs;
 mod tsumego;
 
 /// Declares all routes for the application.
-pub fn declare_routes(conf: &mut actix_web::web::ServiceConfig) {
+pub fn declare_routes(conf: &mut ServiceConfig) {
     auth::declare_routes(conf);
     index::declare_routes(conf);
     srs::declare_routes(conf);
