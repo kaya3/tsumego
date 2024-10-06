dcc2f7f5d4e018a4654922f46b38679ded2f39c0
Andrew Kay <warfreak2@gmail.com>
2024-10-06 16:03:30 +0100

    Review grade buttons

--- frontend/src/view/pages/attempt.ts
+++ frontend/src/view/pages/attempt.ts
@@ -5,9 +5,13 @@ namespace Pages {
         private tsumego: TsumegoData[] = [];
         private index: number = 0;
         
+        // TODO: don't split state between this class and TsumegoView
         private view: TsumegoView;
+        
         private readonly boardContainer: HTMLElement;
         private readonly resultContainer: HTMLElement;
+        private readonly reviewButtonContainer: HTMLElement;
+        private readonly reviewButtons: readonly HTMLButtonElement[];
         private readonly correctSound: HTMLAudioElement;
         private readonly incorrectSound: HTMLAudioElement;
         
@@ -17,6 +21,8 @@ namespace Pages {
             
             this.boardContainer = expectElementById('attempt_board');
             this.resultContainer = expectElementById('result_container');
+            this.reviewButtonContainer = expectElementById('review_buttons');
+            this.reviewButtons = Array.from(this.reviewButtonContainer.getElementsByTagName('button'));
             this.correctSound = expectElementById('correct_sound', 'audio');
             this.incorrectSound = expectElementById('incorrect_sound', 'audio');
         }
@@ -24,60 +30,35 @@ namespace Pages {
         protected hydrate(): void {
             const view = this.view;
             
-            view.onComplete(async win => {
-                // Show a message
-                let resultContainer = this.resultContainer;
-                resultContainer.classList.remove('hidden');
-                if(win) {
-                    resultContainer.classList.add('correct');
-                    resultContainer.innerText = 'Correct!';
+            view.onComplete(async isWin => {
+                // Show a message and play a sound to indicate whether the
+                // solution is correct
+                this.showMessage(isWin);
+                await this.playSound(isWin);
+                
+                if(isWin) {
+                    // Show review buttons
+                    this.reviewButtonContainer.classList.remove('hidden');
+                    for(const button of this.reviewButtons) {
+                        button.disabled = false;
+                    }
                 } else {
-                    resultContainer.classList.remove('correct');
-                    resultContainer.innerText = 'Incorrect';
-                }
-                
-                // Play a sound to indicate whether the solution is correct
-                let sound = win ? this.correctSound : this.incorrectSound;
-                sound.currentTime = 0;
-                await sound.play();
-                
-                // TODO: use finer grades
-                const grade: Grade = win ? 'Easy' : 'Again';
-                // TODO: don't split state between this class and TsumegoView
-                
-                // TODO: don't delay showing next tsumego in case this request is slow
-                // Response will say whether to add this tsumego back into the queue
-                const stats = await API.postReview(this.tsumego[this.index].id, grade);
-                
-                const user = this.app.currentUser;
-                if(user) {
-                    user.reviewsDoneToday++;
-                    // TODO: this isn't exactly right, since this review might
-                    // not have been due; perhaps it was reviewed early, or not
-                    // scheduled at all
-                    if(stats && stats?.srsState.interval >= 1) {
-                        // `interval >= 1` means this tsumego is no longer due today
-                        if(user.reviewsDueToday > 0) {
-                            user.reviewsDueToday--;
-                        }
-                    }
-                }
-                
-                // If this tsumego is due to be reviewed again today, add it
-                // again to the end of the queue
-                if(stats && stats.srsState.interval < 1) {
-                    this.tsumego.push(this.tsumego[this.index]);
-                }
-                
-                this.index++;
-                setTimeout(() => {
-                    if(this.index < this.tsumego.length) {
+                    // Submit review immediately
+                    await this.submitReview('Again');
+                    setTimeout(() => {
                         this.showNextTsumego();
-                    } else {
-                        this.app.navigateHome();
-                    }
-                }, 1000);
+                    }, 1000);
+                }
             });
+            
+            for(const button of this.reviewButtons) {
+                const grade = button.dataset.grade as Grade;
+                
+                button.addEventListener('click', async () => {
+                    await this.submitReview(grade);
+                    this.showNextTsumego();
+                });
+            }
             
             this.boardContainer.appendChild(this.view.canvas);
             function loop() {
@@ -90,6 +71,7 @@ namespace Pages {
         protected onShow(data: TsumegoData[]): void {
             this.tsumego = data;
             this.index = 0;
+            // TODO: ensure the array is non-empty before navigating here
             if(data.length > 0) {
                 this.showNextTsumego();
             }
@@ -101,13 +83,71 @@ namespace Pages {
             this.index = 0;
             this.view.clear();
             this.resultContainer.classList.add('hidden');
+            this.hideReviewButtons();
+        }
+        
+        private showMessage(isWin: boolean): void {
+            let resultContainer = this.resultContainer;
+            resultContainer.classList.remove('hidden');
+            if(isWin) {
+                resultContainer.classList.add('correct');
+                resultContainer.innerText = 'Correct!';
+            } else {
+                resultContainer.classList.remove('correct');
+                resultContainer.innerText = 'Incorrect';
+            }
+        }
+        
+        private async playSound(isWin: boolean): Promise<void> {
+            let sound = isWin ? this.correctSound : this.incorrectSound;
+            sound.currentTime = 0;
+            await sound.play();
+        }
+        
+        private async submitReview(grade: Grade): Promise<void> {
+            const stats = await API.postReview(this.tsumego[this.index].id, grade);
+            
+            const user = this.app.currentUser;
+            if(user) {
+                user.reviewsDoneToday++;
+                // TODO: this isn't exactly right, since this review might
+                // not have been due; perhaps it was reviewed early, or not
+                // scheduled at all
+                if(stats && stats?.srsState.interval >= 1) {
+                    // `interval >= 1` means this tsumego is no longer due today
+                    if(user.reviewsDueToday > 0) {
+                        user.reviewsDueToday--;
+                    }
+                }
+            }
+            
+            // TODO: response should say whether to add this tsumego back into the queue
+            
+            // If this tsumego is due to be reviewed again today, add it
+            // again to the end of the queue
+            if(stats && stats.srsState.interval < 1) {
+                this.tsumego.push(this.tsumego[this.index]);
+            }
+        }
+        
+        private hideReviewButtons(): void {
+            this.reviewButtonContainer.classList.add('hidden');
+            for(const button of this.reviewButtons) {
+                button.disabled = true;
+            }
         }
         
         private showNextTsumego(): void {
-            const data = this.tsumego[this.index];
-            const tsumego = Tsumego.fromData(data);
-            this.view.setTsumego(tsumego);
-            this.resultContainer.classList.add('hidden');
-    }
+            this.index++;
+            if(this.index < this.tsumego.length) {
+                const data = this.tsumego[this.index];
+                const tsumego = Tsumego.fromData(data);
+                this.view.setTsumego(tsumego);
+                this.resultContainer.classList.add('hidden');
+                this.hideReviewButtons();
+            } else {
+                this.app.navigateHome();
+            }
+        }
     }
 }
